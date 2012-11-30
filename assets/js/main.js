56bfff42e7417e0c2dac4f209e01cbd85ca9ba9c
Makis Tracend <makis.tracend@gmail.com>
2012-11-29 16:07:40 -0800

    Adding google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,18 @@ $(function() {
 	window.app = app;
 	// start backbone history
 	Backbone.history.start();
-	
 	// call the localScroll plugin on the document, allowing y scrolling only
 	$.localScroll.defaults.axis = 'y';
-	
-	
 
 });
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-16164477-7']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
