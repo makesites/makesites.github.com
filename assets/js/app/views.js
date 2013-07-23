3272f05c8ce0e1a039104488c36705a41b2c17f8
Lyndel Thomas <lyndelt@gmail.com>
2013-07-23 05:53:11 -0700

    adding logic to change bg

--- assets/js/app/views.js
+++ assets/js/app/views.js
@@ -8,14 +8,21 @@
 		events: {
 			"click a[rel='external']" : "clickExternal",
 			"click .link": "selectLink",
+			"scroll" : "bgChange"
 		},
-		 
-		initialize: function(options){ 
+		
+		
+		 initialize: function(options){ 
 			
 			// every function that uses 'this' as the current object should be in here
-			_.bindAll(this, 'render', 'clickExternal', 'selectLink'); 
+			_.bindAll(this, 'render', 'clickExternal', 'selectLink', 'bgChange');
+			$(window).scroll(this.bgChange); 
 			
 			this.views = {};
+			
+			this.backgrounds = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
+			
+			this.pageHeight = $(window).height();
 			
 			this.views.users = new APP.Views.Users({
 				collection: options.data.users
@@ -48,6 +55,39 @@
 			// return the object for reference
 			return this;
 		}, 
+		
+		
+		bgChange: function() {
+			// pageHeight
+			var st = $(window).scrollTop();
+			var diff = st/(2*this.pageHeight);
+			var index = Math.floor( diff );
+			
+			if(typeof this.backgrounds[index] == "undefined") return;
+			// only update closer to the change of the page
+			//if( (diff-index > 0.98) || (diff-index) < 0.1 ){ 
+			console.log("change");
+				$("body").css('background-image', 'url(/assets/img/'+this.backgrounds[index]+')');
+			//}
+			
+			/*if ( this.checkVisible($(".page#meet") )) {
+				// console.log("You can see me");
+				// console.log($(window).height());
+				// console.lgo()
+				$("body").css('background-image', 'url("http://placekitten.com/1900/1200")');
+				
+			}*/
+			
+			
+		},
+		
+		//checkVisible: function ( elm ) {
+//    		var vpH = $(window).height(), // Viewport Height
+//        	st = $(window).scrollTop(), // Scroll Top
+//        	y = $(elm).offset().top;
+//
+//    		return (y < (vpH + st));
+//		},
 		
 		clickExternal: function(e){
 			e.preventDefault();
