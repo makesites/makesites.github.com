7df41f0a11da4e4a4693fd40bc92d473140e3915
Lyndel Thomas <lyndelt@gmail.com>
2013-08-04 18:29:05 -0700

    removing console logs

--- assets/js/app/views.js
+++ assets/js/app/views.js
@@ -73,7 +73,7 @@
 			var st = $(window).scrollTop();
 			var diff = st/(2*this.pageHeight);
 			var index = Math.floor( diff );
-			console.log(this.pageHeight);
+			
 			if(typeof this.backgrounds[index] == "undefined") return;
 				
 				$("body").css('background-image', 'url(/assets/img/backgrounds/'+this.backgrounds[index]+')');
